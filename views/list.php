cd70158bc634f8286efe976ad3ef1738a63a83eb
Tõnis Ormisson <tonis@andmemasin.eu>
2018-06-25 15:07:21 +0300

    fix missing status

--- views/list.php
+++ views/list.php
@@ -18,12 +18,16 @@ $formatter = new \yii\i18n\Formatter();
         'itemView' =>
             function ($model) use ($formatter) {
                 /** @var $model \andmemasin\myabstract\HasStatusModel */
-                $user = $model->getUserCreated();
-                $title = (empty($model->statusModel->description) ? $model->statusModel->label : $model->statusModel->description);
-                $tag = Html::tag("span", $model->statusModel->label . ' ('. Html::encode($user->username).')', ['data-toggle' => "tooltip", 'title' => $title]);
-                return $formatter->asDatetime($model->timeCreated)
-                    . ' - '
-                    . $tag;
+                if(!empty($model->statusModel)) {
+                    $user = $model->getUserCreated();
+                    $title = (empty($model->statusModel->description) ? $model->statusModel->label : $model->statusModel->description);
+                    $tag = Html::tag("span", $model->statusModel->label . ' ('. Html::encode($user->username).')', ['data-toggle' => "tooltip", 'title' => $title]);
+                    return $formatter->asDatetime($model->timeCreated)
+                        . ' - '
+                        . $tag;
+
+                }
+                return $model->status;
             },
     ]);
     ?>
